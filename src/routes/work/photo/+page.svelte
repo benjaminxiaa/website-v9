7982dafa134067590f4fd2fadff8062ec22a2aa3
Benjamin Xia <xiaa.benjamin@gmail.com>
2024-10-05 12:26:26 -0700

    fixed path bug

--- src/routes/work/photo/+page.svelte
+++ src/routes/work/photo/+page.svelte
@@ -21,18 +21,18 @@
 <div class="outer-container">
 	<div class="after-page">
 		<h2>2022</h2>
-		<a class="link" href="/work/photography/2022/death-valley">death valley</a>
+		<a class="link" href="/work/photo/2022/death-valley">death valley</a>
 		<br />
 		<h2>2023</h2>
-		<a class="link" href="/work/photography/2023/big-sur">big sur</a>
-		<a class="link" href="/work/photography/2023/hakone-gardens">hakone gardens</a>
-		<a class="link" href="/work/photography/2023/new-mexico">new mexico</a>
+		<a class="link" href="/work/photo/2023/big-sur">big sur</a>
+		<a class="link" href="/work/photo/2023/hakone-gardens">hakone gardens</a>
+		<a class="link" href="/work/photo/2023/new-mexico">new mexico</a>
 		<br />
 		<h2>2024</h2>
 		<p>In Progress</p>
 		<!--
-		<span><a class="link" href="/work/photography/2024/south-korea">south korea - seoul</a></span>
-		<span><a class="link" href="/work/photography/2024/fukuoka">japan - fukuoka</a></span>
-		<span><a class="link" href="/work/photography/2024/fukuoka">japan - osaka</a></span> -->
+		<span><a class="link" href="/work/photo/2024/south-korea">south korea - seoul</a></span>
+		<span><a class="link" href="/work/photo/2024/fukuoka">japan - fukuoka</a></span>
+		<span><a class="link" href="/work/photo/2024/fukuoka">japan - osaka</a></span> -->
 	</div>
 </div>
